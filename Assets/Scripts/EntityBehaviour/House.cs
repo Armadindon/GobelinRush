a5bdfa2d33a986bf8a5a5266642d89d2fc4bc3b8
Baptiste Perrin <baptisteperrin77@gmail.com>
2021-06-04 22:45:30 +0200

    Ajout d'un système de vagues

--- Assets/Scripts/EntityBehaviour/House.cs
+++ Assets/Scripts/EntityBehaviour/House.cs
@@ -1,19 +1,69 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
 
 public class House : MonoBehaviour
 {
     [SerializeField]
     private Transform m_enemySpawnPoint;
     [SerializeField]
-    private GameObject m_enemyPrefab;
+    private List<GameObject> m_prefabByEnemy; //On se base sur l'index de l'enum pour choisir un GameObject
+    [SerializeField]
+    private float timeBetweenWaves;
+    [SerializeField]
+    private float timeBetweenEnemy;
+    [SerializeField]
+    private Wave[] m_waves;
 
-    private void Start()
+    private List<GameObject> m_InstanciatedEnemies = new List<GameObject>();
+
+    private int currentWave = 0;
+    private bool isWaveFinished = true;
+
+    private float nextEnemy;
+    private float nextWave;
+
+    private void Awake()
     {
-        if (m_enemyPrefab)
+        //On set up la première wave
+        nextWave = Time.time + timeBetweenWaves;
+    }
+
+    private void Update()
+    {
+        //On vérifie que les gameobjects des ennemis sont toujours là (sinon c'est qu'ils sont dead)
+        m_InstanciatedEnemies = m_InstanciatedEnemies.Where(enemy => enemy != null).ToList();
+
+        //Si on a dépassé le délai, et que il reste des waves
+        if(isWaveFinished && Time.time > nextWave && m_waves.Length != currentWave)
         {
-            Instantiate(m_enemyPrefab, m_enemySpawnPoint.position, Quaternion.identity);
+            Debug.Log("On lance la Wave");
+            isWaveFinished = false;
+        }
+
+
+        //On fait deux if pour que aux même update on puisse instancier le premier ennemis
+        if (m_waves.Length != currentWave && !isWaveFinished && Time.time > nextEnemy && !m_waves[currentWave].isFinished())
+        {
+            Debug.Log("On lance un ennemi");
+            m_InstanciatedEnemies.Add(
+                Instantiate(
+                    m_prefabByEnemy[(int)m_waves[currentWave].getNextEnemy()], 
+                    m_enemySpawnPoint.position, Quaternion.identity)
+                );
+            nextEnemy = Time.time + timeBetweenEnemy;
+        }
+
+
+        //Si la wave est finie et on a tué tous les mobs
+        if (m_waves.Length != currentWave && m_waves[currentWave].isFinished() && m_InstanciatedEnemies.Count == 0)
+        {
+            Debug.Log("On a fini la wave");
+            isWaveFinished = true;
+            nextWave = Time.time + timeBetweenWaves;
+            currentWave++;
         }
     }
+
 }
