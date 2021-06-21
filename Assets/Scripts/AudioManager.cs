7c75cdf0a20983a99771a4bc684f1514318bc4d0
pellanm <mateo.pellan@gmail.com>
2021-06-22 01:13:51 +0200

    ajout des sons pour le spawn, mort et attaque des ennemis ajout des sons pour les tirs de tourelle

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,8 +4,14 @@ using System;
 
 public class AudioManager : MonoBehaviour
 {
+    /// <summary>
+    /// Liste de sons
+    /// </summary>
     public Sound[] sounds;
 
+    /// <summary>
+    /// Instance permettant de ne pas engendrer d'autres managers
+    /// </summary>
     public static AudioManager instance;
 
     void Awake()
@@ -35,6 +41,10 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Va jouer le son avec le nom exact
+    /// </summary>
+    /// <param name="name"></param>
     public void Play(string name)
     {
         //quand utiliser : FindObjectOfType<AudioManager>().Play("nom");
@@ -48,14 +58,20 @@ public class AudioManager : MonoBehaviour
         snd.m_Source.Play();
     }
 
+    /// <summary>
+    /// Va changer le pitch du son demandé en fonction du booléen d'entrée
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="lowerHigher"></param>
     public void ModifyPitch(string name, bool lowerHigher)
     {
-        //on va jouer le son en fonction du nom donné
+        //on va chercher le son en fonction du nom donné
         Sound snd = Array.Find(sounds, sound => sound.name == name);
 
         //si le son ne correspond pas, on ne le joue pas
         if (snd == null) return;
 
+        //change le pitch du son en fonction du booléen d'entrée
         if (lowerHigher)
             snd.m_Source.pitch += 0.5f;
         else
